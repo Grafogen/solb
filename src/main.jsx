f911da78e2531cfa83120468dc997e06148ff603
dtolkachev <dtolkachev.04@gmail.com>
2024-05-06 12:53:31 +0300

    7

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {ChainId, ThirdwebProvider} from "@thirdweb-dev/react";
+import {BrowserRouter, Router} from "react-router-dom";
+import App from "./App";
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+    <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </ThirdwebProvider>
+)
